b8a5f74e642958f1e12142901c52d7451513178a
2ssue <e2ssue@gmail.com>
2020-05-05 19:09:35 +0900

    Refactor: 함수 정의 방식 변경 및 정의 위치 변경
    - 앞에서는 동작만 보여주고, 자세한 동작은 아래에서 확인하도록 하는 것이 더 나을 것 같아서 변경
    - 어떤 것이 더 좋은 방식인지 찾아보는 것도 좋을듯..

--- js/search.js
+++ js/search.js
@@ -3,19 +3,6 @@ import { parseParams } from './utils/parse.js';
 const posts = document.querySelectorAll('.post-list li');
 const postsHeader = document.querySelector('.post-list-heading');
 
-const searchPosts = (value) => {
-  let count = 0;
-  posts.forEach((cur) => {
-    if (!cur.innerText.toUpperCase().includes(value.toUpperCase()))
-      cur.classList.add('unshow');
-    else {
-      count += 1;
-      cur.classList.remove('unshow');
-    }
-  });
-  postsHeader.innerText = `${count}개의 포스트`;
-};
-
 const params = parseParams(document.location.search);
 
 if (params) {
@@ -27,4 +14,15 @@ if (params) {
 document.querySelector('#search-input').addEventListener('keyup', (e) => {
   searchPosts(e.target.value);
 });
+
+function searchPosts(value) {
+  let count = 0;
+  posts.forEach((cur) => {
+    if (!cur.innerText.toUpperCase().includes(value.toUpperCase())) cur.classList.add('unshow');
+    else {
+      count += 1;
+      cur.classList.remove('unshow');
+    }
+  });
+  postsHeader.innerText = `${count}개의 포스트`;
 }
