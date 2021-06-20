295289f340d6d51707bde2680db8d4563c94ce62
2ssue <e2ssue@gmail.com>
2021-06-21 00:12:26 +0900

    :wrench: 로고 이미지 변경, 이미지 플러그인 설정 삭제

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,21 +15,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/images/icon.png',
+        icon: 'public/assets/images/grass.png',
       },
     },
     'gatsby-plugin-mdx',
     'gatsby-transformer-remark',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/images/',
-      },
-      __key: 'images',
-    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
